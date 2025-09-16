a9df2e621178b9c6d3b00c41d2d2a24a0dae3523
Amirhossein Emadi <emadi.henker404@gmail.com>
2025-09-16 05:40:00 -0700

    Add welcome message to the Student Grade Calculator

--- src/main/java/ir/emadi/amir/Main.java
+++ src/main/java/ir/emadi/amir/Main.java
@@ -3,7 +3,14 @@ package ir.emadi.amir;
 public class Main {
 
     public static void main(String... args) {
-        System.out.println("Hi Amirhossein!");
+        welcomeMessage();
+    }
+
+    private static void welcomeMessage() {
+        var message = "* Welcome to the Student Grade Calculator! *";
+        System.out.println("*".repeat(message.length()));
+        System.out.println(message);
+        System.out.println("*".repeat(message.length()) + "\n");
     }
 
 }
