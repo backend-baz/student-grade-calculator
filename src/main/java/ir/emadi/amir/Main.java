cb9062d3b73596f96c4f066934836e3bd4c5b71a
Amirhossein Emadi <emadi.henker404@gmail.com>
2025-09-16 06:12:47 -0700

    Add input handling for grading system selection

--- src/main/java/ir/emadi/amir/Main.java
+++ src/main/java/ir/emadi/amir/Main.java
@@ -1,9 +1,31 @@
 package ir.emadi.amir;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String... args) {
         welcomeMessage();
+        // '1' -> Iran, '2' -> USA and 'null' -> Unknown
+        String typeSystem = null;
+        var scanner = new Scanner(System.in);
+        while (true) {
+            try {
+                if (typeSystem == null) {
+                    System.out.print("Enter your grading system, please -> 1.Iran or 2.USA: ");
+                    typeSystem = scanner.nextLine();
+                    if (!(typeSystem.equals("1") || typeSystem.equals("2"))) {
+                        typeSystem = null;
+                        throw new Exception("Enter 1 or 2, please!");
+                    }
+                }
+                System.out.println();
+                break;
+            } catch(Exception e) {
+                System.out.println("Error -> " + e.getMessage());
+                System.out.println("-".repeat(44));
+            }
+        }
     }
 
     private static void welcomeMessage() {
